f1572b5d565475c64020c956aed48c7142eefd5a
alexandersantosdev <lekxandy@hotmail.com>
2021-07-21 23:00:58 -0300

    Aula 2 curso de VueJS

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 const app = {
     data(){
         return{
-            name: 'Alexander',
-            idade: 34
+            name: '',
+            idade: '',
+            input_name: '',
+            input_idade: ''
         }
-    }
+    },
+    methods: {
+        submitForm(e){
+            e.preventDefault()
+            this.name = this.input_name
+            this.input_name = ''
+            this.idade = this.input_idade
+            this.input_idade = ''
+        }
+    },
 }
 
 Vue.createApp(app).mount("#app")
